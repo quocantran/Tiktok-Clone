c12b6ed39b6a9a11c39b579ab4bccbcce5c2a9e3
quocantran <quocan142536@gmail.com>
2024-03-08 17:53:52 +0700

    format code

--- src/pages/Following/FollowingContent/index.js
+++ src/pages/Following/FollowingContent/index.js
@@ -1,45 +1,45 @@
-import { useEffect, useState } from 'react';
-import Video from '../../../components/Video';
-import request from '../../../ultis/request';
-import Cookies from 'js-cookie';
+import { useEffect, useState } from "react";
+import Video from "../../../components/Video";
+import request from "../../../ultis/request";
+import Cookies from "js-cookie";
 
 const FollowingContent = ({ muted, data, volumeValue }) => {
-    
-    const [isLike,setIsLike] = useState(false);
-    const [isFollowed,setIsFollowed] = useState(false);
-    useEffect(() => {
-        
-            async function getLike(){
-                try{
-                    
-                    const res = await request.get(`/videos/${data.id}`,{
-                        headers : {
-                            Authorization : `Bearer ${Cookies.get('access_token')}`
-                        }
-                    });
-                    setIsLike(res.data.data.is_liked);
-                }
-                catch(err){
-                    
-                }
-               
+  const [isLike, setIsLike] = useState(false);
+  const [isFollowed, setIsFollowed] = useState(false);
+  useEffect(() => {
+    async function getLike() {
+      try {
+        const res = await request.get(`/videos/${data.id}`, {
+          headers: {
+            Authorization: `Bearer ${Cookies.get("access_token")}`,
+          },
+        });
+        setIsLike(res.data.data.is_liked);
+      } catch (err) {}
+    }
+    getLike();
 
-            }
-            getLike();
-            
-            request.get(`/users/@${data.user.nickname}`, {
-                headers : {
-                    Authorization : `Bearer ${Cookies.get('access_token')}`
-                }
-            })
-                .then(res => {
-                    setIsFollowed(res.data.data.is_followed);
-                })
+    request
+      .get(`/users/@${data.user.nickname}`, {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("access_token")}`,
+        },
+      })
+      .then((res) => {
+        setIsFollowed(res.data.data.is_followed);
+      });
+  }, []);
 
-
-    },[])
-    
-    return <Video volumeValue={volumeValue} muted={muted} followingPage={true} followed = {isFollowed} isLike = {isLike} data={data} />;
+  return (
+    <Video
+      volumeValue={volumeValue}
+      muted={muted}
+      followingPage={true}
+      followed={isFollowed}
+      isLike={isLike}
+      data={data}
+    />
+  );
 };
 
 export default FollowingContent;
